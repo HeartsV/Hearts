ccbb3028b1048fb4793be7fdd13cf0d5a2da148e
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-10-17 12:19:57 +0200

    Added Specs for Card Enum and Suit

--- src/main/scala/de/htwg/se/Hearts/Main.scala
+++ src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -5,7 +5,7 @@ import de.htwg.se.Hearts.model.Rank
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val Hand = List(Card(rank = Rank.Five,suit = Suit.Hearts),Card(rank = Rank.Six,suit = Suit.Spades),Card(rank = Rank.King,suit = Suit.Clubs))
+    val Hand = List(Card(rank = Rank.Four,suit = Suit.Hearts),Card(rank = Rank.Six,suit = Suit.Spades),Card(rank = Rank.King,suit = Suit.Clubs))
     println("Please select card to play: ")
     for(element <- 1 until Hand.size + 1){
         printf("|  %d  ",element)
