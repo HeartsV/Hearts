be32cf259b6f1ce2b5885bcd701dc9a3866bb927
xxFlo04 <florian.dunstheimer@gmail.com>
2025-10-17 13:20:20 +0200

    minor changes in test

--- src/test/scala/de/htwg/se/Hearts/model/CardSpec.scala
+++ src/test/scala/de/htwg/se/Hearts/model/CardSpec.scala
@@ -8,15 +8,14 @@ class CardSpec extends AnyWordSpec with Matchers {
 	"A Card" should {
 		val card1 = Card(rank = Rank.Ten, suit = Suit.Hearts)
 		val card2 = Card(rank = Rank.Ace, suit = Suit.Clubs)
-		val card2string = card2.toString
 
 		"have rank and suit" in {
 			card1.rank should be (Rank.Ten)
 			card1.suit should be (Suit.Hearts)
 		}
 		"generate the correct string" in {
-			card2string should include ("\u2663")
-			card2string should include ("A")
+			card2.toString should include ("\u2663")
+			card2.toString should include ("A")
 		}
 	}
 }
