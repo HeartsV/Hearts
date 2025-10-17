366b4a511609ce490f1166f6c7b892e84e82e164
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-10-17 12:57:30 +0200

    Added padding to make sure that outputstrings are always the correct length even for rank 10

--- src/main/scala/de/htwg/se/Hearts/Main.scala
+++ src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -5,10 +5,10 @@ import de.htwg.se.Hearts.model.Rank
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val Hand = List(Card(rank = Rank.Four,suit = Suit.Hearts),Card(rank = Rank.Six,suit = Suit.Spades),Card(rank = Rank.King,suit = Suit.Clubs))
+    val Hand = List(Card(rank = Rank.Ten,suit = Suit.Hearts),Card(rank = Rank.Six,suit = Suit.Spades),Card(rank = Rank.King,suit = Suit.Clubs))
     println("Please select card to play: ")
     for(element <- 1 until Hand.size + 1){
-        printf("|  %d  ",element)
+        printf("|  %d".padTo(7,' '),element)
     }
     println("|")
     for(element<-Hand)
