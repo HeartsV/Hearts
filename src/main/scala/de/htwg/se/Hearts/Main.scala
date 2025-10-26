756debe24010bf340ecead2789bdf6b26d9b95dd
LeReyam <Leon.Mayer@htwg-konstanz.de>
2025-10-26 22:53:54 +0100

    removed mainSpec and removed demo code from main

--- src/main/scala/de/htwg/se/Hearts/Main.scala
+++ src/main/scala/de/htwg/se/Hearts/Main.scala
@@ -5,16 +5,5 @@ import de.htwg.se.Hearts.model.Rank
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val Hand = List(Card(rank = Rank.Ten,suit = Suit.Hearts),Card(rank = Rank.Six,suit = Suit.Spades),Card(rank = Rank.King,suit = Suit.Clubs))
-    println("Please select card to play: ")
-    for(element <- 1 until Hand.size + 1){
-        printf("|  %d".padTo(7,' '),element)
-    }
-    println("|")
-    for(element<-Hand)
-    {
-        printf("| %s ",element)
-    }
-    println("|")
   }
 }
