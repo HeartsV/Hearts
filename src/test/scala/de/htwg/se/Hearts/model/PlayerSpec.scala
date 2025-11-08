ace76832a9ba6da3895c4ef39cc195933f04ebb4
xxFlo04 <florian.dunstheimer@gmail.com>
2025-11-08 16:54:47 +0100

    Tests corrected. Running again.

--- src/test/scala/de/htwg/se/Hearts/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/Hearts/model/PlayerSpec.scala
@@ -22,17 +22,17 @@ class PlayerSpec extends AnyWordSpec with Matchers {
         }
 
         "output handstring" in {
-            p1.handToString() should be ("| A \u2660 | 10\u2665 |")
+            p1.handToString() should be ("|  1  |  2  |\n| A \u2660 | 10\u2665 |")
         }
 
         "be able to play Cards according to the rules" in {
+            p1.playCard(3) should be (false)
+            p1.hand should be (ListBuffer[Card](Card(Rank.Ace,Suit.Spades),Card(Rank.Ten,Suit.Hearts)))
+            Game.trick.cards should be (ListBuffer())
             p1.playCard(2) should be (false)
             p1.hand should be (ListBuffer[Card](Card(Rank.Ace,Suit.Spades),Card(Rank.Ten,Suit.Hearts)))
             Game.trick.cards should be (ListBuffer())
-            p1.playCard(1) should be (false)
-            p1.hand should be (ListBuffer[Card](Card(Rank.Ace,Suit.Spades),Card(Rank.Ten,Suit.Hearts)))
-            Game.trick.cards should be (ListBuffer())
-            p1.playCard(0) should be (true)
+            p1.playCard(1) should be (true)
             p1.hand should be (ListBuffer[Card](Card(Rank.Ten,Suit.Hearts)))
             Game.trick.cards should be (ListBuffer[Card](Card(Rank.Ace,Suit.Spades)))
             Game.trick.addCard(Card(Rank.Two,Suit.Spades))
