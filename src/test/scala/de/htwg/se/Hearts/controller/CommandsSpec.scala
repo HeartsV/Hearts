ac4e7d69d006211af8f687fe0a35ec2044d5e795
xxFlo04 <florian.dunstheimer@gmail.com>
2026-01-13 14:13:07 +0100

    refactor: enhance CommandsSpec with additional command tests and cleanup, but tests not fully implemented

--- src/test/scala/de/htwg/se/Hearts/controller/CommandsSpec.scala
+++ src/test/scala/de/htwg/se/Hearts/controller/CommandsSpec.scala
@@ -10,15 +10,19 @@ class CommandsSpec extends AnyWordSpec with Matchers {
 	val gameCo = Controller(game)
 
 	"A redoCommand" should {
-		"" in {}
+		"" in {
+
+		}
 	}
 
 	"A undoCommand" should {
+		"" in {
 
+		}
 	}
 
 	"A SetPlayerNumberCommand" should {
-		"set the number of players"in {
+		"set the number of players" in {
 		val command = SetPlayerNumberCommand(index = Some (3))
 		val commandf = SetPlayerNumberCommand(index = Some (5))
 		command.setup(gameCo)
@@ -37,45 +41,88 @@ class CommandsSpec extends AnyWordSpec with Matchers {
 	}
 
 	"A SetMaxScoreCommand" should {
+		"set the max score" in {
+		val command = SetMaxScoreCommand(index = Some (3))
+		val commandf = SetMaxScoreCommand(index = Some (450))
 
+		command.setup(gameCo)
+		command.storeBackup
+		gameCo.processInput(commandf)
+		gameCo.game.getMaxScore should be (Some(100))
+		gameCo.undo
+		gameCo.game.getMaxScore should be (None)
+		gameCo.processInput(command)
+		gameCo.game.getMaxScore should be (Some(3))
+		gameCo.undo
+		gameCo.game.getMaxScore should be (None)
+		}
 	}
 
 	"A SetSortingRankCommand" should {
+		"set sorting to rank" in {
+			gameCo.processInput(SetSortingRankCommand())
 
+		}
 	}
 
 	"A SetSortingSuitCommand" should {
+		"" in {
+			gameCo.processInput(SetSortingSuitCommand())
 
+		}
 	}
 
 	"A NewCommand" should {
+		"" in {
+			gameCo.processInput(NewCommand())
 
+		}
 	}
 
 	"A AgainCommand" should {
+		"" in {
+			gameCo.processInput(AgainCommand())
 
+		}
 	}
 
 	"A QuitCommand" should {
+		"" in {
+			gameCo.processInput(QuitCommand())
 
+		}
 	}
 
 	"A ExitCommand" should {
+		"" in {
+			gameCo.processInput(ExitCommand())
 
+		}
 	}
 
 	"A RulesCommand" should {
+		"" in {
+			gameCo.processInput(RulesCommand())
 
+			}
 	}
 
 	"A BackCommand" should {
+		"" in {
+			gameCo.processInput(BackCommand())
 
+		}
 	}
 
 	"A ContinueCommand" should {
+		"" in {
+			gameCo.processInput(ContinueCommand())
 
+		}
 	}
 	"A PlayCardCommand" should {
+		"" in {
 
+		}
 	}
 }
