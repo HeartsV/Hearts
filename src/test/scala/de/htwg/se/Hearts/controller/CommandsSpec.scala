64ccc75d8e71deef40b4ba2c27fd3e43bf18ba93
xxFlo04 <florian.dunstheimer@gmail.com>
2026-01-13 13:51:32 +0100

    test: enhance SetPlayerNumberCommand tests with additional input scenario to cover the case when the if condition is not met

--- src/test/scala/de/htwg/se/Hearts/controller/CommandsSpec.scala
+++ src/test/scala/de/htwg/se/Hearts/controller/CommandsSpec.scala
@@ -20,8 +20,11 @@ class CommandsSpec extends AnyWordSpec with Matchers {
 	"A SetPlayerNumberCommand" should {
 		"set the number of players"in {
 		val command = SetPlayerNumberCommand(index = Some (3))
+		val commandf = SetPlayerNumberCommand(index = Some (5))
 		command.setup(gameCo)
 		command.storeBackup
+		gameCo.processInput(commandf)
+		gameCo.game.getPlayerNumber should be (None)
 		gameCo.processInput(command)
 		gameCo.game.getPlayerNumber should be (Some(3))
 		gameCo.undo
