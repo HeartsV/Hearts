cd79b81816533ace744ae23f28d834c371cc009c
xxFlo04 <florian.dunstheimer@gmail.com>
2026-01-20 02:25:24 +0100

    Update file I/O binding to use JSON implementation instead of XML

--- src/main/scala/de/htwg/se/Hearts/HeartsModule.scala
+++ src/main/scala/de/htwg/se/Hearts/HeartsModule.scala
@@ -34,7 +34,7 @@ class HeartsModule extends AbstractModule with ScalaModule {
 		bind(classOf[LeaderBoardInterface]).to(classOf[LeaderBoard])
 		bind(classOf[DirectorInterface]).to(classOf[Director])
 		bind(classOf[CoRInterface]).to(classOf[ChainOfResponsibility])
-		bind(classOf[FileIOInterface]).to(classOf[fileXMLImpl.FileIO])
+		bind(classOf[FileIOInterface]).to(classOf[fileIOJSONImpl.FileIO])
 
 		@Provides
 		def providePlayer(): PlayerInterface =
